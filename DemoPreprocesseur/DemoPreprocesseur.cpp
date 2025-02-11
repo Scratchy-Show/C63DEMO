5b3bcb93f107184197d8f600d681b0b870d04d4d
Couret Alexandre <couretalexandredev@gmail.com>
2025-02-11 10:43:34 -0500

    Ajout de commentaires

--- DemoPreprocesseur/DemoPreprocesseur.cpp
+++ DemoPreprocesseur/DemoPreprocesseur.cpp
@@ -1,37 +1,39 @@
-#include <iostream>
+#include <iostream> // Inclut la bibliothèque standard pour l'entrée et la sortie (ex. cin, cout).
 #include "Patate.h"
 // On évite une erreure de compilation grâce au #pragma once
-#include "Patate.h"
+#include "Patate.h" // Inclut encore une fois "Patate.h", ce qui est redondant, mais #pragma once protège contre la duplication d'inclusion.
 
-using namespace std;
+using namespace std; // Permet d'utiliser directement les symboles du namespace standard (comme cout, endl) sans devoir écrire std:: à chaque fois.
 
 // #include copie-colle le contenu entier d'un fichier
 // En pratique, sert à importer la déclaration de symboles
 int main()
 {
     // Utilisation d'une variable standard
-    Patate p;
-    #include "NeFaisPasCa.h";
+    Patate p;  // Crée un objet de la classe "Patate"
+    #include "NeFaisPasCa.h";  // Inclut un fichier "NeFaisPasCa.h" dans le code.
     std::cout << "Hello World!\n";
-    std::cout << "PI: " << PI << endl;
-    std::cout << "Circonference de 4.0f: " << FORMULE_CIRCONFERENCE(4.0f) << endl;
+    std::cout << "PI: " << PI << endl;  // Affiche la valeur de la constante PI
+    std::cout << "Circonference de 4.0f: " << FORMULE_CIRCONFERENCE(4.0f) << endl;  // Calcule et affiche la circonférence d'un cercle de rayon 4.0f en utilisant la macro/formule FORMULE_CIRCONFERENCE.
     //std::cout << "Circonference de Coucou!: " << FORMULE_CIRCONFERENCE("Coucou!") << endl;
 
 
     std::cout << "Fichier: " << __FILE__ << " Ligne: " << __LINE__ << std::endl;
 
-// Détruie la variable
-#undef GAME_RELEASE
+    // Détruie la variable
+    #undef GAME_RELEASE
 
-// Décide s'il met ou non ce code et ensuite il compile
-#ifdef GAME_RELEASE
-    std::cout << "GAME RELEASE" << endl;
-#else
-    std::cout << "GAME DEBUG" << endl;
-#endif
+    // Décide s'il met ou non ce code et ensuite il compile
+    // Condition de compilation : ce bloc sera inclus seulement si "GAME_RELEASE" est défini.
+    #ifdef GAME_RELEASE
+        std::cout << "GAME RELEASE" << endl; // Affiche "GAME RELEASE" si "GAME_RELEASE" est défini.
+    #else
+        std::cout << "GAME DEBUG" << endl; // Affiche "GAME DEBUG" si "GAME_RELEASE" n'est pas défini.
+    #endif
 
-// Décide s'il met ou non ce code et ensuite il compile
-#ifndef STEAM_VERSION
-    std::cout << "N'est pas sur Steam" << endl;
-#endif
+    // Décide s'il met ou non ce code et ensuite il compile
+    // Condition de compilation : ce bloc sera inclus seulement si "STEAM_VERSION" n'est pas défini.
+    #ifndef STEAM_VERSION
+        std::cout << "N'est pas sur Steam" << endl; // Affiche "N'est pas sur Steam" si "STEAM_VERSION" n'est pas défini.
+    #endif
 }
--- DemoPreprocesseur/Patate.h
+++ DemoPreprocesseur/Patate.h
@@ -1,28 +1,29 @@
-//#pragma once
+//#pragma once  // Cette ligne est commentée. #pragma once est une directive de préprocesseur qui garantit qu'un fichier d'en-tête n'est inclus qu'une seule fois dans chaque unité de traduction.
+
 
 // Remplace #pragma once
-//#ifndef __PATATE_H
-//#define __PATATE_H
-//#endif
+//#ifndef __PATATE_H  // Début d'une directive conditionnelle. Si __PATATE_H n'est pas défini, le code entre #ifndef et #endif sera exécuté.
+ //#define __PATATE_H  // Définit __PATATE_H pour indiquer que le fichier a été inclus, et que ce code ne doit plus être réinclus.
+ //#endif  // Fin de la directive conditionnelle, en fait commentée ici mais elle est parfois utilisée pour éviter les inclusions multiples.
 
+#ifndef __PATATE_H  // Vérifie si __PATATE_H n'a pas encore été défini.
+#define __PATATE_H  // Définit __PATATE_H, ce qui signifie que ce fichier d'en-tête a déjà été inclus et qu'il ne sera pas réinclus.
 
-#ifndef __PATATE_H
-#define __PATATE_H
-
-// Define: Crée des variables pré-processeur (macro)
-#define GAME_RELEASE
+// Define: Crée des variables pré-processeur (macros)
+#define GAME_RELEASE  // Définit la macro GAME_RELEASE, cette ligne peut être utilisée plus tard pour conditionner le code selon l'état de compilation (version de production ou débogage).
 // Copie colle la valeur
-#define PI 3.1415f
+#define PI 3.1415f  // Définit la constante PI à 3.1415f (utilisé pour des calculs mathématiques, comme le calcul de la circonférence).
 
-#define FORMULE_CIRCONFERENCE(r) 2.f*PI*r
-#define ADDITION(a,b) a+b
+#define FORMULE_CIRCONFERENCE(r) 2.f*PI*r  // Définit une macro qui calcule la circonférence d'un cercle en fonction de son rayon 'r'. Utilise PI pour effectuer le calcul.
+#define ADDITION(a,b) a+b  // Définit une macro qui effectue l'addition de deux valeurs 'a' et 'b'.
 
 // Conneries
-// #define true false
-// #define while while(true)
+// #define true false  // (Commentée) Cela pourrait remplacer la valeur booléenne 'true' par 'false', ce qui rendrait tout programme utilisant 'true' se comporter de manière erronée.
+// #define while while(true)  // (Commentée) Cela transformerait chaque boucle 'while' en une boucle infinie. C'est une "connerie" car elle causerait un comportement indésirable.
 
-class Patate
+class Patate  // Déclaration de la classe "Patate". Elle est vide ici, mais pourrait être remplie avec des membres (variables, fonctions, etc.).
 {
 
 };
-#endif
+
+#endif  // Fin de la directive conditionnelle #ifndef __PATATE_H, ce qui marque la fin du bloc de code qui ne sera inclus qu'une seule fois.
